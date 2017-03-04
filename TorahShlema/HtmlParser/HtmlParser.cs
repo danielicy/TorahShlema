51303a584ac0e2c36b1960019c26808015ac3587
danieli <danielicy@hotmail.com>
2017-03-04 23:23:57 +0200

    HtmlParserUtility

---
+++ TorahShlema/HtmlParser/HtmlParser.cs
@@ -0,0 +1,48 @@
+﻿
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Windows.Forms;
+using System.Xml;
+
+
+namespace HtmlParser
+{
+    public class HtmlParserUtility
+    {
+        string[] _files;
+        public int LoadFiles()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, @"SourceFiles\Torah\Cantillated"); 
+            _files = Directory.GetFiles(path);
+
+            foreach (string file in _files)
+            {
+
+              
+
+                HtmlWeb doc = new HtmlWeb();
+               
+                var loadedDoc = doc.Load(file);
+                var html = loadedDoc.DocumentNode.InnerHtml;
+
+                HtmlNodeCollection nodeCollection = loadedDoc.DocumentNode.SelectNodes("//b");
+
+                foreach (HtmlNode node in nodeCollection)
+                {
+                   
+
+                }
+
+
+                  }
+
+                return 0;
+        }
+    }
+}
---
+++ TorahShlema/TorahShlema/Program.cs
@@ -0,0 +1,19 @@
+﻿using HtmlParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlParser;
+
+namespace TorahShlema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlParserUtility parser = new HtmlParserUtility();
+            parser.LoadFiles();
+        }
+    }
+}
