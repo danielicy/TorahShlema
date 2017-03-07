dd3ccb1d2e2656ac1602079932892c6c6a058b5e
danieli <danielicy@hotmail.com>
2017-03-07 20:47:15 +0200

    Can Parse Verses

--- TorahShlema/HtmlParser/HtmlParser.cs
+++ TorahShlema/HtmlParser/HtmlParser.cs
@@ -1,4 +1,5 @@
 ﻿
+using EllisWeb.Gematria;
 using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 //using System.Windows.Forms;
 using System.Xml;
-
+using TorahShlema.Models.Models;
 
 namespace HtmlParser
 {
@@ -18,45 +19,50 @@ namespace HtmlParser
         string[] _files;
         public int LoadFiles()
         {
-            string path = Path.Combine(Environment.CurrentDirectory, @"SourceFiles\Torah\Cantillated"); 
+            string path = Path.Combine(Environment.CurrentDirectory, @"SourceFiles\Torah\Cantillated\Unkulus"); 
             _files = Directory.GetFiles(path);
 
             foreach (string file in _files)
             {
-
-              
-
                 HtmlWeb doc = new HtmlWeb();
                
                 var loadedDoc = doc.Load(file);
-                var html = loadedDoc.DocumentNode.InnerHtml;
+               
+                HtmlNodeCollection nodeCollection = loadedDoc.DocumentNode.SelectNodes("//td");
 
-                HtmlNodeCollection nodeCollection = loadedDoc.DocumentNode.SelectNodes("//p/text()");
-
-                string chapteNumberr;
-                string verseNumber;
-                string verse;
-
+                Torah torah;
+                int firstposition, lastposition,lenght;
                 if (nodeCollection != null)
                 {
                     foreach (HtmlNode node in nodeCollection)
                     {
-                      
-                        try {
-                        chapteNumberr = node.PreviousSibling.InnerText.Split(',')[0];
-                        verseNumber = node.PreviousSibling.InnerText.Split(',')[1];
-                            verse = node.InnerText.Replace("\n", "");
-                                }
+                        torah = new Torah();
+                        try
+                        {
+                            if (!string.IsNullOrEmpty(node.InnerText))
+                            {
+                                lastposition = node.InnerText.Length- node.InnerText.IndexOf('׃')-1  ;
+
+                                //indicates where to start text from
+                                firstposition = node.InnerText.IndexOf(',')+1 ;
+                                lenght = node.InnerText.Length  - firstposition-lastposition-3;
+                                torah.HebVerse = node.InnerText.Replace("\n", "").Substring(firstposition, lenght);
+                                torah.Unkulus = node.NextSibling.InnerText.Replace("\n", "").Replace(".", "");
+                                torah.ChapterNUmber = Calculator.GetNumericGematriaValue(node.ChildNodes[3].InnerText.Split(',')[0]); //node.InnerText.Replace("\n", ""),GematriaType.Absolute);
+                                torah.VerseNumber = Calculator.GetNumericGematriaValue(node.ChildNodes[3].InnerText.Split(',')[1]);
+                                torah.divition = node.InnerText.Substring(node.InnerText.IndexOf('{'), 3);
+                            }
+                               
+                        }
                         catch { }
 
                     }
-
                 }
-
-
             }
-
                 return 0;
         }
     }
+
+        
+  
 }
---
+++ TorahShlema/TorahShlema.Models/Models/Torah.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorahShlema.Models.Models
+{
+    public class Torah
+    {
+        public int BookId { get; set; }
+
+        public int ChapterNUmber { get; set; }
+
+        public string HebVerse { get; set; }
+
+        public string Unkulus { get; set; }
+
+        public int VerseNumber { get; set; }
+
+        public string divition { get; set; }
+    }
+}
